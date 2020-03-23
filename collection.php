76959f47d7e0d5f8157cdc3b4e84c5c3aed438c9
MaxG <mh.granger@yahoo.co.uk>
2020-03-23 16:29:02 +0000

    Output data for users

--- collection.php
+++ collection.php
@@ -3,5 +3,28 @@
 $db = new PDO('mysql:host=DB;dbname=collection', 'root', 'password');
 
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-$query = $db->query('SELECT `name` FROM `characterinfo`;');
-$data = $query->fetchAll();
+$query = $db->query('SELECT * FROM `characterinfo` JOIN `houses` ON `characterinfo`.`house` = `houses`.`id`;');
+$hpcharacters = $query->fetchAll();
+
+?>
+
+<!DOCTYPE html>
+    <body>
+        <main>
+            <h1>Harry Potter Characters</h1>
+            <div>
+
+                <?php
+
+                foreach ($hpcharacters as $character) {
+                    echo '<h3>' . $character["name"] . '</h3>';
+                    echo '<p>' . $character["blood-type"] . '</p>';
+                    echo '<p>' . $character["house"] . '</p>';
+                }
+
+                ?>
+
+            </div>
+        </main>
+    </body>
+</html>
